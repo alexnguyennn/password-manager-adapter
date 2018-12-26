e5c2d55d143adbc3f7d222af34a4156f8c96e0f0
Unknown <alex-nguyen@live.com.au>
2018-12-26 22:48:26 +1100

    project setup

---
+++ PasswordManager.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PasswordManager.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
